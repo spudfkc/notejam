ed6509ce4a62354d76acad0a3f3fd86f096d40c7
ncc <nick@complion.com>
2016-10-31 19:13:55 -0400

    npm tests

--- express/notejam/db2.js
+++ express/notejam/db2.js
@@ -6,32 +6,27 @@ function queryAsPromised(query) {
   return new Promise(function(resolve, reject) {
     db.query(query,
       function(err, result) {
-        console.log('in callback')
         if (err) {
           console.log('got err', err)
           reject(err);
         }
-        console.log('resolving', result)
         resolve(result);
       });
   });
 }
 
 function createTables() {
-  console.log('starting')
   return queryAsPromised("CREATE TABLE IF NOT EXISTS users (" +
       "id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL," +
       "email VARCHAR(75) NOT NULL," +
       "password VARCHAR(128) NOT NULL);")
   .then(function(result) {
-    console.log('created users')
     return queryAsPromised("CREATE TABLE IF NOT EXISTS pads (" +
       "id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL," +
       "name VARCHAR(100) NOT NULL," +
       "user_id INTEGER NOT NULL REFERENCES users(id));")
    })
   .then(function(result) {
-    console.log('created pads')
     return queryAsPromised("CREATE TABLE IF NOT EXISTS notes (" +
             "id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL," +
             "pad_id INTEGER REFERENCES pads(id)," +
@@ -53,10 +48,9 @@ function truncateTables() {
 
 db.connect(err => {
   if(err) console.log(err);
-  console.log('connected')
   return createTables().then(function() {
     console.log('tables created');
-    db.end(() => console.log('ended'));
+    db.end(() => console.log('done'));
   }).catch(err => console.error('FAILED', err))
   db.destroy();
   process.end()
